641bea0bf9c2bf004594dba880d2d73156bf9da7
Andréa <andrealeticiatavares@gmail.com>
2022-11-08 22:01:10 -0300

    Modelando API

--- src/main/java/br/com/codebank/controller/AccountController.java
+++ src/main/java/br/com/codebank/controller/AccountController.java
@@ -1,4 +1,7 @@
 package br.com.codebank.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AccountController {
 }
--- src/main/java/br/com/codebank/controller/CustomerController.java
+++ src/main/java/br/com/codebank/controller/CustomerController.java
@@ -1,4 +1,13 @@
 package br.com.codebank.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CustomerController {
+    @GetMapping("/Customer")
+    public String Customer(){
+        return "Olá";
+    }
+
 }
