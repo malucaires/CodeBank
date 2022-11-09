6f1e7d15b241cd4b17f705c699c5d62875625581
malucaires <malucairess@gmail.com>
2022-11-08 21:21:40 -0300

    add spring initilizr

---
+++ src/main/java/br/com/codebank/CodebankApplication.java
@@ -0,0 +1,13 @@
+package br.com.codebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodebankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodebankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/codebank/CodebankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodebankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
